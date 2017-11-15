5bcb9242a367b4fc57f04dca5a03c7d7a0c6307a
Nicolas Molina Monroy <nicolas.molina.monroy@gmail.com>
2017-11-15 14:11:54 -0400

    add todo action

---
+++ src/redux/todo/todo.actions.ts
@@ -0,0 +1,15 @@
+import { Action } from '@ngrx/store';
+
+import { Todo } from './todo.model';
+
+export const TODO_ADD = '[Todos] add';
+
+export class TodoAddAction implements Action {
+  readonly type = TODO_ADD;
+
+  constructor(
+    public payload: Todo
+  ) {}
+}
+
+export type AllActions = TodoAddAction;
